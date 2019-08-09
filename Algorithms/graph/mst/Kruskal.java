ce735e14ae7513b64ba61be6dc8efb02596f2842
Liu Zeyao <zeyao621@gmail.com>
2019-08-09 22:14:19 +0800

    correct union find logic

--- Algorithms/graph/mst/Kruskal.java
+++ Algorithms/graph/mst/Kruskal.java
@@ -4,18 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
-import java.util.Set;
 
 public class Kruskal {
 	/**
 	 * 
 	 * 和prime不同的是，KrusKal在所有的边来找最小距离的边，
-	 * 每次加入的边不能让图有环，通过判断要加入的边的两点是不是已经联通（如果已经联通，再去加上这条边，就有环啦）
+	 * 每次加入的边不能让图有环，通过判断要加入的边的两点是不是已经联通（如果已经联通，再去加上这条边，就有环啦）这里用到union find 的思想
 	 * 把所有有效边就是mst
 	 *
 	 */
@@ -37,7 +35,14 @@ public class Kruskal {
 				return o1.length - o2.length;                 
 			}
 		};
-		Map<String,Set<String>> unionMap = new HashMap<>();
+		     
+        Map<String,String> unionMap = new HashMap<>();
+        //union map for key with node name and value with the unionId
+        for (Connection c : connectionList) {
+        	unionMap.put(c.node1, c.node1);
+        	unionMap.put(c.node2, c.node2);
+        }
+        
 		List<Connection> mst = new ArrayList<>();
 		PriorityQueue<Connection> pq = new PriorityQueue<>(compareFactor);
 		//对于边的优先队列
@@ -48,64 +53,58 @@ public class Kruskal {
 			Connection minLenconnection = pq.poll();
 			String node1 = minLenconnection.node1;
 			String node2 = minLenconnection.node2;
-			if (isUnioned(node1, node2, unionMap)) {
+			if (union(node1, node2, unionMap)) {
 				continue;
 			}
 			mst.add(minLenconnection);
-			
-			//add union for two node of this connection
-			Set<String> node1AdjList = unionMap.get(node1);
-			Set<String> node2AdjList = unionMap.get(node2);
-			node1AdjList.add(node2);
-			node2AdjList.add(node1);
-			
+		
 		}		
 		return mst;		
 	}
 	
-	private static boolean isUnioned(String node1, String node2, Map<String,Set<String>> unionMap) {
-		Set<String> node1AdjList;
-		Set<String> node2AdjList;
-		if (!unionMap.containsKey(node1)) {
-			node1AdjList = new HashSet<>();
-			unionMap.put(node1, node1AdjList);
-		}
-		else {
-			node1AdjList = unionMap.get(node1);
-		}
-		if (!unionMap.containsKey(node2)) {
-			node2AdjList = new HashSet<>();
-			unionMap.put(node2, node1AdjList);
-		}
-		else {
-			node2AdjList = unionMap.get(node2);
-		}
-		
-		if (node2AdjList.contains(node1) || node1AdjList.contains(node2)) {
-			return true;
-		}
-		for (String node1Adj : node1AdjList) {
-			if (node2AdjList.contains(node1Adj)) {
-				return true;
-			}
-		}
-		return false;
-	}
+	/**
+	 * 判断两点是不是联通，用unionMap来记录每个node已经联通的node，如果两个node1 node2任何AdjList有任何一个相同的node说明node1 node2是联通的
+	 */
+	private static String find(String node, Map<String,String> unionMap) {
+        return unionMap.get(node);
+    }
+    
+    private static boolean union(String p, String q, Map<String,String> unionMap) {
+        String pId = find(p, unionMap);
+        String qId = find(q, unionMap);
+        if (pId.equals(qId)) {
+            return true;
+        }
+        
+        for (Map.Entry<String,String> entry : unionMap.entrySet()) {
+            if (pId.equals(entry.getValue())) {
+            	entry.setValue(qId);
+            }
+        }
+        return false;
+    }
 	
 	public static void main(String[] args) {
-        Connection c1 = new Connection("A", "D", 1);
-        Connection c2 = new Connection("A", "B", 3);
-        Connection c3 = new Connection("D", "B", 3);
-        Connection c4 = new Connection("B", "C", 1);
-        Connection c5 = new Connection("C", "D", 1);
-        Connection c6 = new Connection("E", "D", 6);
-        Connection c7 = new Connection("C", "E", 5);
-        Connection c8 = new Connection("C", "F", 4);
-        Connection c9 = new Connection("E", "F", 2);
-        List<Connection> graph = new ArrayList<>(Arrays.asList(c1, c2, c3, c4, c5, c6, c7, c8, c9));
-        List<Connection> result = KrusKalMst(graph);
-        for (Connection conn : result) {
-            System.out.println(conn.node1 + "-" + conn.node2);
-        }
+		 Connection n1 = new Connection("1", "5", 32);
+	        Connection n2 = new Connection("5", "4", 35);
+	        Connection n3 = new Connection("5", "7", 28);
+	        Connection n4 = new Connection("4", "7", 37);
+	        Connection n5 = new Connection("1", "7", 19);
+	        Connection n6 = new Connection("1", "3", 29);
+	        Connection n7 = new Connection("3", "2", 17);
+	        Connection n8 = new Connection("2", "7", 34);
+	        Connection n9 = new Connection("1", "2", 36);
+	        Connection n10 = new Connection("3", "6", 52);
+	        Connection n11 = new Connection("2", "6", 40);
+	        Connection n12 = new Connection("0", "7", 16);
+	        Connection n13 = new Connection("0", "2", 26);
+	        Connection n14 = new Connection("0", "4", 38);
+	        Connection n15 = new Connection("0", "6", 58);
+	        Connection n16 = new Connection("4", "6", 93);
+	        List<Connection> graph2 = new ArrayList<>(Arrays.asList(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, n16));
+	        List<Connection> result2 = KrusKalMst(graph2);
+	        for (Connection conn : result2) {
+	            System.out.println(conn.node1 + "-" + conn.node2);
+	        }
     }	
 }
