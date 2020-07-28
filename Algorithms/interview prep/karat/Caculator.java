9e0a89c5914d25614ec7d2ef20716b88b0c2cf61
Liu Zeyao <zeyao621@gmail.com>
2020-07-28 20:37:45 +0800

    calculator

---
+++ Algorithms/interview prep/karat/Caculator.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Caculator {
+
+    //Pt.1 Calculator without parenthesis, only +, -, non-negative ints
+    public static int basicCalculator1(String str) {
+        int sum = 0;
+        boolean plus = true;
+        int index = 0;
+        while (index < str.length()) {
+            StringBuilder sb = new StringBuilder();
+            while (index < str.length() && Character.isDigit(str.charAt(index))) {
+                sb.append(str.charAt(index));
+                index++;
+            }
+            int num = Integer.parseInt(sb.toString());
+            if (plus) {
+                sum += num;
+            }
+            else {
+                sum -= num;
+            }
+            if (index >= str.length()) {
+                break;
+            }
+ 
+            if (str.charAt(index) == '+') {
+                plus = true;
+            }
+            else if (str.charAt(index) == '-') {
+                plus = false;
+            }
+            index++;           
+        }
+        return sum;
+    }
+
+    //Pt.2 Calculator with parenthesis (LeetCode 224)
+    public static int calculate2(String s) {
+        int res = 0;
+        int plus = 1;
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        while (i < s.length()) {
+            if (Character.isDigit(s.charAt(i))) {
+                String numStr = "";
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    numStr += s.charAt(i);
+                    i++;
+                }
+                int num = Integer.parseInt(numStr);
+                res += num * plus;                
+            }
+            if (i >= s.length()) {
+                break;
+            }
+            if (s.charAt(i) == '+') {
+                plus = 1;
+            }
+            else if (s.charAt(i) == '-') {
+                plus = -1;
+            }
+            else if (s.charAt(i) == '(') {
+                stack.add(res);
+                stack.add(plus);
+                plus = 1;
+                res = 0;
+            }
+            else if (s.charAt(i) == ')') {
+                int operator = stack.pop();
+                int lastNum = stack.pop();
+                res = res * operator + lastNum;
+            }
+            i++;
+        }
+        return res;
+    }
+    public static void main(String[] args) {    
+        System.out.println(basicCalculator1("1+43+78") == 1+43+78);
+        System.out.println(basicCalculator1("1+430-30+1") == 1+430-30+1);
+        System.out.println(basicCalculator1("1+473+787+112+5645+7676+24123") == 1+473+787+112+5645+7676+24123);
+        System.out.println(basicCalculator1("132+473+786667+1222+5645+7676+24123") == 132+473+786667+1222+5645+7676+24123);
+
+
+
+        System.out.println(calculate2("132+473+786667-(1222-5645+7676)+24123") == 132+473+786667-(1222-5645+7676)+24123);
+        System.out.println(calculate2("(1+132)-(473-7667)-(1222-5645+7676)+24123") == (1+132)-(473-7667)-(1222-5645+7676)+24123);
+    }
+}
+
